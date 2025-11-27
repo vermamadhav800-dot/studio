36f0077c199a70d46666ef2adc95121c5c88a6c6
PRANAV VERMA <pvverma0214@gmail.com>
2025-11-27 17:26:54 +0000

    npx shadcn@latest add @react-bits/ScrollReveal-JS-CSS
    USAGE
    import Scro

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -1,5 +1,6 @@
 import { Badge } from '@/components/ui/badge';
 import { Section } from '@/components/section';
+import ScrollReveal from '../ui/ScrollReveal';
 
 const skills = [
   'React',
@@ -20,10 +21,24 @@ export default function Skills() {
   return (
     <Section id="skills">
       <div className="text-center mb-12">
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl font-headline">Skills & Expertise</h2>
-        <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed mt-4">
+        <ScrollReveal
+          containerClassName='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl font-headline'
+          textClassName='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl font-headline'
+          baseRotation={0}
+          enableBlur={false}
+          baseOpacity={0}
+          >
+          Skills & Expertise
+        </ScrollReveal>
+        <ScrollReveal
+          containerClassName='mx-auto max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed mt-4'
+          textClassName='mx-auto max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed mt-4'
+          baseRotation={0}
+          enableBlur={false}
+          baseOpacity={0}
+        >
           My technical toolkit for building modern web experiences.
-        </p>
+        </ScrollReveal>
       </div>
       <div className="flex flex-wrap justify-center gap-4">
         {skills.map((skill) => (
